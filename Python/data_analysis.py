a9f5952c5082dae76e0cc39e193af2ea8b721398
ALEXIS ELLIOTT <lexi@ALEXISs-MacBook-Air.local>
2023-04-05 17:42:00 -0500

    first submission

--- Python/data_analysis.py
+++ Python/data_analysis.py
@@ -135,6 +135,7 @@ def graph_all_attributes(csv_file: str):
         plt.savefig(file_name)
         plt.close()
 
+
 def graph_attribute(attribute: str, trial: str):
 
     '''
@@ -165,7 +166,7 @@ def graph_attribute(attribute: str, trial: str):
     plt.plot(df5['time'], df5[attribute], label = files[4])
     plt.plot(df6['time'], df6[attribute], label = files[5])
 
-    plt.title(attribute)
+    plt.title(attribute + ' trail0' + trial)
     plt.xlabel('time')
     # set y-axis unit
     if 'rot' in attribute:
@@ -180,6 +181,7 @@ def graph_attribute(attribute: str, trial: str):
     plt.legend()
     plt.savefig(file_name)
     plt.close()
+
 
 def graph_all_attributes(trial: str):
     '''
@@ -209,36 +211,94 @@ def graph_all_attributes(trial: str):
     for attribute in attributes:
         graph_attribute(attribute, trial)
 
-graph_all_attributes('1')
+
+def summarize_significant_attributes(signif_attributes):
+    '''
+    This function prints tables that have computed the mean and variance for the 
+    inputed significant attributes for all activties types
+    Input:
+        signif_attributes : a list of strings representing the selected 
+                            significant attributes
+    '''
+
+    print('\n')
+    activities = ['standing', 'sitting', 'jogging', 'arms chopping', 
+                  'arms stretching', 'twisting']
+    for activity in activities:
+        print(activity)
+        summary = activity_attribute_trends(activity)
+        summary = summary.loc[signif_attributes]
+        print(summary)
+        print('\n')
 
 
-# graph_all_attributes('STD_01.csv')
-# graph_all_attributes('SIT_01.csv')
-# graph_all_attributes('JOG_01.csv')
-# graph_all_attributes('CHP_01.csv')
-# graph_all_attributes('STR_01.csv')
-# graph_all_attributes('TWS_01.csv')
-        
+def graph_signif_attributes(attributes, trial_num: int):
+    '''
+    This function graphs the significant attributes provided across all
+    activities and saves the graphs into the Graphs folder within my lab
+    directory, creating a new graph for each trial.
 
-#summarize_sensor_trace('STD_01.csv')
+    Input:
+        attributes: a list of strings representing the selected 
+                    significant attributes
+        trial_num (str): the number of trials being graphed
+    '''
+    for index in range(trial_num):
+        trial = str(index + 1)
+        for attribute in attributes:
+            graph_attribute(attribute, trial)
 
-# visualize_sensor_trace('STD_01.csv', 'headset_rot.y')
-# visualize_sensor_trace('STD_01.csv', 'headset_rot.x')
 
-# tws1 = summarize_sensor_trace('TWS_01.csv')
-# chp1 = summarize_sensor_trace('CHP_01.csv')
-# print(tws1)
-# print(chp1)
+def dif_controller_yposition(csv_file_1: str, csv_file_2: str, trial: str):
+    '''
+    This function graphs difference in the y psoition between the right and left
+    controller as a function of time between two difference activities for one 
+    trial and saves the graph into the Graphs folder within my lab directory.
 
-# print("standing")
-# print(activity_attribute_trends("standing"))
-# print("sitting")
-# print(activity_attribute_trends("sitting"))
-# print("jogging")
-# print(activity_attribute_trends("jogging"))
-# print("arms chopping")
-# print(activity_attribute_trends("arms chopping"))
-# print("arms stretching")
-# print(activity_attribute_trends("arms stretching"))
-# print("twisting")
-# print(activity_attribute_trends("twisting"))
+    Input:
+        csv_file_1 (str): the csv file of sensor data for the first activity being 
+                    plotted
+        csv_file_2 (str): the csv file of sensor data for the second activity being 
+        plotted
+        trial (str): the trial number being graphed
+    '''
+
+    file_name = '../Graphs/difference_in_controller_pos.y_trial0' + trial + '.png'
+    f = open(file_name, 'w') # clear the file if there is existing data
+
+    df1 = pd.read_csv(f'../Data/Lab1/{csv_file_1}', index_col=False)
+    df2 = pd.read_csv(f'../Data/Lab1/{csv_file_2}', index_col=False)
+    left1 = df1.loc[:,'controller_left_pos.y']
+    right1 = df1.loc[:,'controller_right_pos.y']
+    left2 = df2.loc[:,'controller_left_pos.y']
+    right2 = df2.loc[:,'controller_right_pos.y']
+
+    dif1 = []
+    dif2 = []
+
+    for index,value in enumerate(left1):
+        dif1.append(left1[index] - right1[index])
+
+    for index,value in enumerate(left2):
+        dif2.append(left2[index] - right2[index])
+
+    dif1 = pd.Series(dif1)
+    dif2 = pd.Series(dif2)
+
+    data = {
+        'time' : df1.loc[:,'time'],
+        csv_file_1 : dif1,
+        csv_file_2 : dif2
+    }
+
+    accel_df = pd.DataFrame(data)
+    accel_df.plot(x='time', y=[csv_file_1, csv_file_2])
+    plt.ylabel('meters')
+    plt.title('difference in controller position trial 0' + trial)
+    plt.legend()
+    plt.savefig(file_name)
+
+
+signif_attr = ['controller_right_pos.y', 'controller_left_pos.y',
+                'controller_right_pos.z', 'controller_left_pos.z', 
+                'headset_pos.y', 'headset_pos.z']
